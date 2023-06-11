c66fa12f7d03c0647b3029e402fe0a1a9da7449e
olegtsvetkov332 <tsvetkovoleg798@gmail.com>
2023-06-11 13:18:28 +0300

    ДЗ С#  семинар 7

---
+++ Z_2/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+Console.Write("Введите колличество строк в двумерном массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов в двумерном массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+
+void GenerateRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void PrintRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+GenerateRandomArray(numbers);
+PrintRandomArray(numbers);
+
+Console.Write("Введите номер строки в двумерном массиве: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца в двумерном массиве: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void Position(int[,] array, int row, int col)
+{
+    if (row > array.GetLength(0) || col > array.GetLength(1))
+    {
+        Console.Write("Такого числа в массиве нет");
+    }
+    else
+    {
+        Console.Write($"Значение элемента в строке {row} в столбце {col} равно {array[row, col]}");
+    }
+}
+Position(numbers, num1, num2);
---
+++ Z_3/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Write("Введите колличество строк в двумерном массиве: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите колличество столбцов в двумерном массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[m, n];
+
+void GenerateRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+}
+
+void ArithmeticMean(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {   
+        double Result = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Result += array[i, j];
+        }
+        Console.Write($"{Result / array.GetLength(0)}; ");
+    }
+}
+
+GenerateRandomArray(numbers);
+PrintRandomArray(numbers);
+Console.WriteLine($"");
+ArithmeticMean(numbers);
